025496e95a86313c077d3264955478381c88d99a
emartinez <emartinez@f188eadf-6e79-4af6-b0bb-63c603171115>
2009-05-13 23:13:01 +0000

    Abstracted NSHM Oracle DB authentication credentials.

---
+++ nshm/src/nshm_api.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+#include "adhoc_api.h"
+#include "nshm_agrid.h"
+#include "nshm_agrid_meta.h"
+
+
+AdHoc_AuthInfo NSHM_AUTH[] = {
+	{"teamt", "haz_owner", "ham23*ret", "teamt", ""},
+	{"team2", "haz_owner", "ham23*ret", "team2", ""},
+	{"pub1", "haz_owner", "ham23*ret", "pub1", ""}
+};
