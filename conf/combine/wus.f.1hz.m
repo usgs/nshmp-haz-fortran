5eebe230cee5986329ef1df3f0fa791d0c26c30e
pmpowers <pmpowers@f188eadf-6e79-4af6-b0bb-63c603171115>
2011-08-12 21:42:30 +0000

    test conf commit

---
+++ conf/combine/wus.f.1hz.m
@@ -0,0 +1,46 @@
+1
+22
+../out/brange.3dip.ch.1hz.m
+0.6666
+../out/brange.3dip.gr.1hz.m
+0.3334
+../out/brange.3dip.65.1hz.m
+1.0000
+../out/nv.3dip.ch.1hz.m
+0.6666
+../out/nv.3dip.gr.1hz.m
+0.3334
+../out/nvut.3dip.65.1hz.m
+1.0000
+../out/ut.3dip.ch.1hz.m
+0.6666
+../out/ut.3dip.gr.1hz.m
+0.3334
+../out/orwa_c.1hz.m
+0.5000					ch/gr get full wgt in file
+../out/orwa_n.3dip.ch.1hz.m
+0.6667
+../out/orwa_n.3dip.gr.1hz.m
+0.3333
+../out/wasatch.3dip.ch.1hz.m
+1.0000					dip branch wgts in file are 0.144 + 0.432 + 0.144 = 0.72
+../out/wasatch.3dip.gr.1hz.m
+1.0000					dip branch wgts in file are 0.036 + 0.108 + 0.036 = 0.18
+../out/wasatch.3dip.74.1hz.m
+1.0000					dip branch wgts in file are 0.02 + 0.06 + 0.02 = 0.1
+../out/bFault.ch.1hz.m
+0.6667					faults have combined wgt of 1 in file
+../out/bFault.gr.1hz.m
+0.1667					faults have combined wgt of 2 in file
+../out/aFault_aPriori_D2.1.1hz.m
+0.4500
+../out/aFault_unseg.1hz.m
+0.0500
+../out/aFault_MoBal.1hz.m
+0.2250
+../out/creepflt.1hz.m
+1.0000
+1
+../out/combine/tmp.wus.f.1hz.m
+0
+1
---
+++ conf/combine/wus.f.5hz.p
@@ -0,0 +1,46 @@
+1
+22
+../out/brange.3dip.ch.5hz.p
+0.6666
+../out/brange.3dip.gr.5hz.p
+0.3334
+../out/brange.3dip.65.5hz.p
+1.0000
+../out/nv.3dip.ch.5hz.p
+0.6666
+../out/nv.3dip.gr.5hz.p
+0.3334
+../out/nvut.3dip.65.5hz.p
+1.0000
+../out/ut.3dip.ch.5hz.p
+0.6666
+../out/ut.3dip.gr.5hz.p
+0.3334
+../out/orwa_c.5hz.p
+0.5000					ch/gr get full wgt in file
+../out/orwa_n.3dip.ch.5hz.p
+0.6667
+../out/orwa_n.3dip.gr.5hz.p
+0.3333
+../out/wasatch.3dip.ch.5hz.p
+1.0000					dip branch wgts in file are 0.144 + 0.432 + 0.144 = 0.72
+../out/wasatch.3dip.gr.5hz.p
+1.0000					dip branch wgts in file are 0.036 + 0.108 + 0.036 = 0.18
+../out/wasatch.3dip.74.5hz.p
+1.0000					dip branch wgts in file are 0.02 + 0.06 + 0.02 = 0.1
+../out/bFault.ch.5hz.p
+0.6667					faults have combined wgt of 1 in file
+../out/bFault.gr.5hz.p
+0.1667					faults have combined wgt of 2 in file
+../out/aFault_aPriori_D2.1.5hz.p
+0.4500
+../out/aFault_unseg.5hz.p
+0.0500
+../out/aFault_MoBal.5hz.p
+0.2250
+../out/creepflt.5hz.p
+1.0000
+1
+../out/combine/tmp.wus.f.5hz.p
+0
+1
