541c8b06e6b7658ddf29557f2dd55841f9fb3d61
mmoschetti <mmoschetti@f188eadf-6e79-4af6-b0bb-63c603171115>
2013-09-20 18:17:33 +0000

    modified all-combine file

--- scripts/combine2014_all.sh.sh
+++ scripts/combine2014_all.sh.sh
@@ -67,21 +67,47 @@ if (( $run_WUS_grids )); then
 ../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix.g.pga
+#
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad.g.pga
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix.g.pga
+#
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad.g.pga
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix.g.pga
+#
 ../bin/hazallXL.v2 ../conf/combine/wus_puget.g.5hz
 ../bin/hazallXL.v2 ../conf/combine/wus_puget.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/wus_puget.g.pga
+# Mmax=8 files
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_ad_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_ad_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_ad_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_EXTmap_fix_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_ad_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_WUSmap_fix_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_ad_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_noPuget_fix_M8.g.pga
+../bin/hazallXL.v2 ../conf/combine/wus_puget_M8.g.1hz
+../bin/hazallXL.v2 ../conf/combine/wus_puget_M8.g.5hz
+../bin/hazallXL.v2 ../conf/combine/wus_puget_M8.g.pga
+
 # shear zones
 ../bin/hazallXL.v2 ../conf/combine/shear_zones.g.1hz
 ../bin/hazallXL.v2 ../conf/combine/shear_zones.g.5hz
